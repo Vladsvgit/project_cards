d83c56dffc9f751d90794f88ac50b5f1a58744a7
Yas-X <yasx80@gmail.com>
2023-05-11 01:13:31 +0300

    Create Visits - add Inputs+Information

--- src/js/constant.js
+++ src/js/constant.js
@@ -12,5 +12,13 @@ export const titleInput = document.querySelector(".main__createVisit__title");
 export const discriptionInput = document.querySelector(".main__createVisit__discription");
 export const urgencyInput = document.querySelector(".main__createVisit__urgency");
 export const nameInput = document.querySelector(".main__createVisit__name");
+export const preassureInput = document.querySelector(".main__createVisit__pressure");
+export const bmiInput = document.querySelector(".main__createVisit__bmi");
+export const heartIllInput = document.querySelector(".main__createVisit__heartIll");
+export const ageInput = document.querySelector(".main__createVisit__age");
+export const lastVisitInput = document.querySelector(".main__createVisit__lastVisit");
+export const createVisitBtn = document.querySelector(".main__createVisit__btn");
+
+
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-import { loginBtn, createBtn, loginWind, unloginBtn, filterVisit, chooseDoctor, titleInput, discriptionInput, urgencyInput, nameInput } from "./constant.js";
+import { loginBtn, createBtn, loginWind, unloginBtn, filterVisit, chooseDoctor, titleInput, discriptionInput, urgencyInput, nameInput, preassureInput, bmiInput, heartIllInput, ageInput, lastVisitInput, createVisitBtn, createVisitWind } from "./constant.js";
 
 import { login } from "./login.js";
 
@@ -16,15 +16,50 @@ unloginBtn.addEventListener("click", (e) => {
     unloginBtn.classList.add("displNone");
     loginBtn.classList.remove("displNone");
     filterVisit.classList.add("displNone");
+    createVisitWind.classList.add("displNone");
+    document.querySelector(".main__message").remove();
 });
 
 chooseDoctor.addEventListener("change", e => {
     e.preventDefault();
-    console.log(titleInput);
-    titleInput.classList.remove(".displNone");
-    discriptionInput.classList.remove(".displNone");
-    urgencyInput.classList.remove(".displNone");
-    nameInput.classList.remove(".displNone");
+
+    if (e.target.value === "Cardiologist") {
+        titleInput.classList.remove("displNone");
+        discriptionInput.classList.remove("displNone");
+        urgencyInput.classList.remove("displNone");
+        nameInput.classList.remove("displNone");
+        preassureInput.classList.remove("displNone");
+        bmiInput.classList.remove("displNone");
+        heartIllInput.classList.remove("displNone");
+        ageInput.classList.remove("displNone");
+        lastVisitInput.classList.add("displNone");
+        createVisitBtn.classList.remove("displNone");
+        return;
+    } else if (e.target.value === "Dentist") {
+        titleInput.classList.remove("displNone");
+        discriptionInput.classList.remove("displNone");
+        urgencyInput.classList.remove("displNone");
+        nameInput.classList.remove("displNone");
+        preassureInput.classList.add("displNone");
+        bmiInput.classList.add("displNone");
+        heartIllInput.classList.add("displNone");
+        ageInput.classList.add("displNone");
+        lastVisitInput.classList.remove("displNone");
+        createVisitBtn.classList.remove("displNone");
+        return;
+    } else if (e.target.value === "Therapist") {
+        titleInput.classList.remove("displNone");
+        discriptionInput.classList.remove("displNone");
+        urgencyInput.classList.remove("displNone");
+        nameInput.classList.remove("displNone");
+        preassureInput.classList.add("displNone");
+        bmiInput.classList.add("displNone");
+        heartIllInput.classList.add("displNone");
+        lastVisitInput.classList.add("displNone");
+        ageInput.classList.remove("displNone");
+        createVisitBtn.classList.remove("displNone");
+        return;
+    }
 
     console.log(e.target.value);
 })
